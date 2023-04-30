506030c13472d5d3fdcc329be356258a044b1673
Benjamin Leveque <104634127+BenBishop40@users.noreply.github.com>
2023-04-30 18:44:42 +0200

    debug loc save DB et gestion isUser / display messages

--- 02-chatbox-react-app/src/App.js
+++ 02-chatbox-react-app/src/App.js
@@ -7,6 +7,7 @@ import { useParams } from "react-router-dom";
 // Firebase
 import base from "./base";
 import { onValue, ref, set, remove } from "firebase/database";
+// import { limitToLast } from "firebase/database";
 
 function App() {
     const pseudoURL = useParams();
@@ -25,7 +26,9 @@ function App() {
 
     useEffect(() => {
         // Création d'une référence à l'emplacement des messages dans la base de données Firebase
-        const messagesRef = ref(base, `/messages/${pseudo}`);
+        const messagesRef = ref(base, "/");
+        // ne fonctionne pas la limite de messages.....!!!!!
+        // const lastMessagesRef = limitToLast(messagesRef.orderByKey(), 10);
 
         // Ecouter les modifications de données en temps réel à cet emplacement dans la base de données Firebase
         onValue(messagesRef, (snapshot) => {
@@ -33,12 +36,12 @@ function App() {
             if (data) {
                 setMessages(data);
 
-                // Suppression des messages anciens au delà de 10
+                // Suppression des messages anciens au delà de 20
                 const messagesKeys = Object.keys(data);
-                if (messagesKeys.length > 10) {
-                    const messagesToDelete = messagesKeys.slice(0, messagesKeys.length - 10);
+                if (messagesKeys.length > 20) {
+                    const messagesToDelete = messagesKeys.slice(0, messagesKeys.length - 20);
                     messagesToDelete.forEach((key) => {
-                        const messageRef = ref(base, `/messages/${pseudo}/${key}`);
+                        const messageRef = ref(base, `/${key}`);
                         remove(messageRef);
                     });
                 }
@@ -46,23 +49,33 @@ function App() {
         });
     }, [pseudo]);
 
-    // fonction ajout message avec key unique "message-timestamp"
+    // fonction ajout message avec definition key unique "message-timestamp"
     const addMessage = (message) => {
         const newKey = `message-${Date.now()}`;
 
+        // Ecriture message ds dB Firebase
+        set(ref(base, `/${newKey}`), message);
         // Mise à jour state messages
         setMessages((messages) => ({ ...messages, [newKey]: message }));
-        // Ecriture message ds dB Firebase
-        set(ref(base, `/messages/${pseudo}/${newKey}`), message);
     };
+
+    // identification boolen sur pseudo message = pseudo URL
+    const isUser = (pseudo) => pseudo === pseudoURL.pseudo;
 
     return (
         <div className="box">
             <div>
                 <div className="messages" ref={messageRefScroll}>
-                    {Object.keys(messages).map((key) => (
-                        <Message key={key} message={messages[key].message} pseudo={messages[key].pseudo} />
-                    ))}
+                    <div className="message">
+                        {Object.keys(messages).map((key) => (
+                            <Message
+                                key={key}
+                                isUser={isUser}
+                                message={messages[key].message}
+                                pseudo={messages[key].pseudo}
+                            />
+                        ))}
+                    </div>
                 </div>
             </div>
             <Formulaire addMessage={addMessage} pseudo={pseudo} length={140} />
