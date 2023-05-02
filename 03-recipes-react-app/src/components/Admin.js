b48fb762f95377e005bfd578ce262bf4cc300606
Benjamin Leveque <104634127+BenBishop40@users.noreply.github.com>
2023-05-02 16:21:59 +0200

    config Facebook login oK - SDK configuré et login OK tested

--- 03-recipes-react-app/src/components/Admin.js
+++ 03-recipes-react-app/src/components/Admin.js
@@ -1,9 +1,36 @@
 import React, { Component } from "react";
 import AddRecette from "./AddRecette";
+import Login from "./Login";
+
+// import firebase / authentification
+// import firebase from "firebase/app";
+import { FacebookAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import "firebase/auth";
+import { firebaseApp } from "../base";
 
 class Admin extends Component {
+    state = {
+        uid: null,
+        chef: null,
+    };
+
+    handleAuth = (authData) => {
+        console.log(authData);
+    };
+
+    authenticate = () => {
+        const provider = new FacebookAuthProvider();
+        const auth = getAuth(firebaseApp);
+        signInWithPopup(auth, provider).then(this.handleAuth);
+    };
+
     render() {
         const { addRecette, chargerExemple } = this.props;
+
+        // Si utilisateur non connecté
+        if (!this.state.uid) {
+            return <Login authenticate={this.authenticate} />;
+        }
         return (
             <div className="cards">
                 <AddRecette addRecette={addRecette}></AddRecette>
