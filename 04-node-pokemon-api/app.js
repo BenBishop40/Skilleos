3c4c19275fb4323f7ca4a35fddca7fc6909e9626
Benjamin Leveque <104634127+BenBishop40@users.noreply.github.com>
2023-05-31 16:15:00 +0200

    ajout get HTTP pokemon et collection nber total

--- 04-node-pokemon-api/app.js
+++ 04-node-pokemon-api/app.js
@@ -1,1 +1,20 @@
-console.log("hello api");
+const express = require("express"); // recherche dépendance express
+const pokemons = require("./mock-pokemon");
+const app = express(); // création server express
+const port = 3000; // définition du port sur lequel toune l'app
+
+app.get("/", (req, res) => res.send("Hello, Express"));
+
+// Profil pokemon selon id parametre
+app.get(`/api/pokemons/:id`, (req, res) => {
+    const id = parseInt(req.params.id);
+    const pokemon = pokemons.find((pokemon) => pokemon.id === id);
+    res.send(`Hello, vous avez demandé le pokémon ${pokemon.name}`);
+});
+
+// Synthèse Nbre pokemons :
+app.get("/api/pokemons", (req, res) => {
+    res.send(`Il y a ${pokemons.length} pokémons dans le pokédex, pour le moment.`);
+});
+
+app.listen(port, () => console.log(`Application Node démarrée sur port ${port}...`)); // démarrage api rest sur port 3000 et renvoi du log
